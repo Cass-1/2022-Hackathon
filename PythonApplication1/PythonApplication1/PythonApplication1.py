54815a6d9ddbd7aa5778a211116c5c404cb2740e
Isaac Dahle <isaacbd.personal@gmail.com>
2022-02-19 17:59:00 -0800

    Finished most of the auxilery functions. I got it working with one room. So now im working on setting up all the rooms.

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -59,56 +59,138 @@ class Person:
     def __str__(self):
         return self.name
 
-def enter_Room(room, actions):
+
+#checks if movement is valid
+def check_movement(currentRoom, movement):
+    newRoom = currentRoom.go_direction(movement)
+
+    if(newRoom == currentRoom):
+        return False, currentRoom
+    else:
+        return True, newRoom
+
+#what to do when a new room is entered
+def enter_room(room, actions):
 
     print(room.description)
    
+    
+#Summary: gets and validates an action from the player
+#Parameters: the current room and a list of actions to take
+#Returns: the action chosen
+#Precond: none
+#Postcond: the returned userInput will be one of the valid actions
+def get_action(room, actions):
+    
+    #successfulMovement = -1
+
     for string in room.actions:
         print(string)
 
     #get user input
-    while True:
+    
+    userInput = input(": ")
+
+    #validate user input
+    while userInput not in actions:
+        print("Not a valid action")
         userInput = input(": ")
-        while userInput not in actions:
-           print("Not a valid action")
-           userInput = input(": ")
-        if userInput == "talk":
-            print(room.personInRoom.conversation())
-        elif userInput == "forward":
-            break
-        elif userInput == "back":
-            break
-        elif userInput == "right":
-            break
-        elif userInput == "left":
-            break
+
+    return userInput
+
+        
+        
+#Summary: allows the player to move or talk
+#Parameters: the current room and a user input action
+#Returns: the new room the player is in
+#Precond: user input has to be in the allowed actions
+#Postcond: a newRoom has been returned
+def talk_and_move(currentRoom, userInput):
+
+    successfulMovement = False
+    nextStepStr = ""
+    
+    #what to do based on input
+    if userInput == "talk":
+        print(currentRoom.personInRoom.conversation())
+        nextStepStr = "You have now talked to everyone here"
+    elif userInput == "forward":
+        successfulMovement, newRoom = check_movement(currentRoom, "forward")
+                
+    elif userInput == "back":
+        successfulMovement, newRoom = check_movement(currentRoom, "back")
+            
+    elif userInput == "right":
+        successfulMovement, newRoom = check_movement(currentRoom, "right")
+            
+    elif userInput == "left":
+        successfulMovement, newRoom = check_movement(currentRoom, "left")
+        
+    if successfulMovement == True:
+        nextStepStr = "Moved rooms"
+        return newRoom, nextStepStr
+    else:
+        nextStepStr = "Rememeber, you're late! Please enter a room you can travel too"
+        return currentRoom, nextStepStr
+
+
+
+def main():
+    #People
+    sparkManagerTopics = ["The weather sure is nice!", "You should stop by and grab some coffee!"]
+    sparkDeskManager = Person("Mark", "Desk Manager",sparkManagerTopics , False)
+
+    studyingStudentTopics = ["I have miderms soon!", "Hush! I'm trying to study"]
+    studyingStudent = Person("Samantha", "Student",studyingStudentTopics , False)
+
+    cryingStudentTopics = ["Thanks for talking with me. I've had a very long day", "I really appreciate you talking to me. It's been nice to see a friendly face!"]
+    cryingStudent = Person("Robert", "Student",cryingStudentTopics , True)
+
+    sleepingStudentTopics = ["Zzzz....", "Mghrh....zzzzz.."]
+    sleepingStudent = Person("Jane", "Student",sleepingStudentTopics , False)
+
+
+    #Room Descriptions
+    entranceDescription = "You pass through the large main doors and find yourslef inside of The Spark. You glance around and notice a person sitting at the front desk are greated with a large staircase forward, a cafe to your left, and an exit behind you."
+
+
+    #People in each room and room actions
+    entrancePeople = {sparkDeskManager}
+    actions = ["talk", "forward", "back", "right", "left"]
+    
+    
+    #Room Objects
+    entrance = Room("Entrance", entranceDescription, sparkDeskManager, actions)
+
+    #Setting Directions
+    
+    
+
+    currentRoom = entrance
+
+    newRoom = currentRoom
+
+    nextStepStr = ""
+
+    #entering rooms
+    enter_room(currentRoom, currentRoom.actions)
+
+    #talking with people and moving between rooms
+    while newRoom == currentRoom:
+        userInput = get_action(currentRoom, currentRoom.actions)
+        newRoom, nextStepStr = talk_and_move(currentRoom, userInput)
+        print(nextStepStr)
         
 
 
 
 
-def main():
-    #desk manager
-    entranceDescription = "You pass through the large main doors and find yourslef inside of The Spark. You glance around and notice a person sitting at the front desk are greated with a large staircase forward, a cafe to your left, and an exit behind you."
-    sparkManagerTopics = ["The weather sure is nice!", "You should stop by and grab some coffee!"]
-    
-    sparkDeskManager = Person("Mark", "Desk Manager",sparkManagerTopics , False)
-
-    #entrance room
-    entrancePeople = {sparkDeskManager}
-    actions = ["talk", "forward", "back", "right", "left"]
-    entrance = Room("Entrance", entranceDescription, sparkDeskManager, actions)
-
-    currentRoom = entrance
-
-    #entering rooms
-    enter_Room(currentRoom, currentRoom.actions)
 
 
-
-
-
-
-
+#Summary: 
+#Parameters: 
+#Returns: 
+#Precond:
+#Postcond:
 
 main()
