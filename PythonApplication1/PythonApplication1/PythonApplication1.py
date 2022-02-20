235a56d2539da392e0f7c6f99fc9eb2c2d1221de
Isaac Dahle <isaacbd.personal@gmail.com>
2022-02-19 17:21:08 -0800

    Added some code to enter_room(). Still working on it

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,18 +1,20 @@
 
 #a comment
 import random as r
+import copy
+
 
 class Room:
     
-    def __init__ (self, name, description,peopleInRoom, actions, forward=None, left=None, right=None, back= None):
+    def __init__ (self, name, description,personInRoom, actions, forward=None, left=None, right=None, back= None):
         self.name = name
         self.forward = forward
         self.left = left
         self.right = right
         self.back = back
         self.description = description #a blurb once u enter the room
-        self.peopleInRoom = peopleInRoom #a list of the people in the room
-        self.actions = actions # a list of the actions you can take(strings)
+        self.personInRoom = personInRoom #a list of the people in the room
+        self.actions = copy.deepcopy(actions) # a list of the actions you can take(strings)
 
         if forward == None:
             self.forward = self
@@ -49,30 +51,58 @@ class Person:
         self.inDistress = inDistress
 
     def conversation(self):
-        convoString = r.randint(0, self.topics.len())
+        randInt = r.randint(0, len(self.topics) - 1)
+        convoString = self.topics[randInt]
+        #print(convoString)
         return convoString
 
     def __str__(self):
         return self.name
 
-def enter_Room(room):
+def enter_Room(room, actions):
+
     print(room.description)
-    print(room.actions)
+   
+    for string in room.actions:
+        print(string)
+
+    #get user input
+    while True:
+        userInput = input(": ")
+        while userInput not in actions:
+           print("Not a valid action")
+           userInput = input(": ")
+        if userInput == "talk":
+            print(room.personInRoom.conversation())
+        elif userInput == "forward":
+            break
+        elif userInput == "back":
+            break
+        elif userInput == "right":
+            break
+        elif userInput == "left":
+            break
+        
+
 
 
 
 def main():
-    print("in main")
+    #desk manager
     entranceDescription = "You pass through the large main doors and find yourslef inside of The Spark. You glance around and notice a person sitting at the front desk are greated with a large staircase forward, a cafe to your left, and an exit behind you."
-    sparkDeskManager = {"Mark", "Desk Manager", {"The weather sure is nice!", "You should stop by and grab some coffee!"}, False}
-    entrancePeople = {sparkDeskManager}
-    actions = {"talk", "forward", "back", "right", "left"}
+    sparkManagerTopics = ["The weather sure is nice!", "You should stop by and grab some coffee!"]
     
+    sparkDeskManager = Person("Mark", "Desk Manager",sparkManagerTopics , False)
 
-    entrance = {"Entrance", entranceDescription, entrancePeople, actions}
+    #entrance room
+    entrancePeople = {sparkDeskManager}
+    actions = ["talk", "forward", "back", "right", "left"]
+    entrance = Room("Entrance", entranceDescription, sparkDeskManager, actions)
+
     currentRoom = entrance
 
-    enter_Room(currentRoom)
+    #entering rooms
+    enter_Room(currentRoom, currentRoom.actions)
 
 
 
